d6405159f30df8e3fa301e210e0bfec5f34b828a
Ian W. Scott <iscott@tyndale.ca>
2014-02-01 10:33:41 -0500

    merged with remote

--- modules/plugin_widgets.py
+++ modules/plugin_widgets.py
@@ -201,8 +201,6 @@ def MODAL(triggertext, headertext, body,
 
 def ACCORDION(panels, id='my_accordion'):
     '''
-    Each member of the panels argument (a list) should itself be a list or
-    tuple containing four members:
     [0]     pid
     [1]     panel link text
     [2]     panel content
@@ -213,14 +211,14 @@ def ACCORDION(panels, id='my_accordion'):
         pid = panel[0]
         linktext = panel[1]
         content = panel[2]
-        bclasses = '' if len(panel) <= 3 else panel[3]
+        bclasses = panel[3] if len(panel) > 3 else ''
         linkattrs = {'_class': "accordion-toggle {}-toggle".format(pid),
                      '_data-toggle': "collapse",
                      '_data-parent': "#{}".format(id),
                      '_href': "#{}".format(pid)}
         headattrs = {'_class': "accordion-heading"}
-        bodyattrs = {'_class': 'accordion-body '}
-        innerattrs = {'_class': 'accordion-inner collapse {}'.format(bclasses), '_id': pid}
+        bodyattrs = {'_class': 'accordion-body collapse {}'.format(bclasses)}
+        innerattrs = {'_class': 'accordion-inner'}
         groupattrs = {'_class': "accordion-group"}
         p = DIV(DIV(A(linktext, **linkattrs), **headattrs),
                 DIV(DIV(content, **bodyattrs), **innerattrs),
